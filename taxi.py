95da002da34915f5be7eff1975fbc02501047748
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-09-30 11:44:35 +0200

    in progress

--- taxi.py
+++ taxi.py
@@ -9,13 +9,8 @@ D=2.7
 def course(distance=0,tarif=C,minimum=prise_en_charge,supplement=0):
   return (distance * tarif) + minimum + supplement
 
-prix = course(distance=52, tarif=C)
-print(prix)
-prix = course(distance=52, tarif=C, supplement=2)
-print(prix)
-prix = course(distance=52, tarif=A)
-print(prix)
-prix = course(distance=40, tarif=A)
-print(prix)
-prix = course(distance=60, tarif=A)
-print(prix)
+print(course(distance=52, tarif=C))
+print(course(distance=52, tarif=C, supplement=2))
+print(course(distance=52, tarif=A))
+print(course(distance=40, tarif=A))
+print(course(distance=60, tarif=A))
