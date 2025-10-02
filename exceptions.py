aada428849c80baa63d056ac117f6ca0400fe55d
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-10-02 16:38:29 +0200

    in progress

--- exceptions.py
+++ exceptions.py
@@ -1,31 +1,39 @@
-var = input('Entrez une valeur')
+# il. faut séparer les gestions d'erreur petit à petit:
+# 1 traitement, une gestion d'erreur,
+# et on répète comme ça étape par étape pour une bonne gestion
+# des erreurs et des exécutions
+
+var = input("Entrez une valeur")
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 i = True
 while i:
-  try:
-    value = 42 / float(var)
-    print(value)
+    try:
+        value = 42 / float(var)
+        print(value)
 
-    print(alphabet[int(var)])
-    i = False
+        print(alphabet[int(var)])
+        i = False
 
-  except FloatingPointError as err:
-    print(f"Float? t'es sûr? {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
-  except ValueError as err:
-    print(f"Oulha ce n'est pas la bonne valeur {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
-  except ZeroDivisionError as err:
-    print(f"Division par zéro impossible {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
-  except TypeError as err:
-    print(f"Type error: {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
-  except IndexError as err:
-    print(f"Mauvais index dans l'alphabet: {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
+    except FloatingPointError as err:
+        print(f"Float? t'es sûr? {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
+    except ValueError as err:
+        print(f"Oulha ce n'est pas la bonne valeur {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
+    except ZeroDivisionError as err:
+        print(f"Division par zéro impossible {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
+    except TypeError as err:
+        print(f"Type error: {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
+    except IndexError as err:
+        print(f"Mauvais index dans l'alphabet: {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
 
-  except Exception as err:
-    print(f"Unexpected {err=}, {type(err)=}")
-    var = input('Entrez une valeur')
-    raise
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        var = input("Entrez une valeur")
+        raise
+
+    finally:
+        print("Hey what else ?")
