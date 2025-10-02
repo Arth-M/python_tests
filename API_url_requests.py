55658d88e1448929237cabce3265b4f209a585b4
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-10-02 12:04:17 +0200

    in progress

--- API_url_requests.py
+++ API_url_requests.py
@@ -1,10 +1,12 @@
 # import requests #pour les API
-import urllib.request # pour scrapper
-import smtplib # pour les mails
-import pprint #pretty print
-import logging # pour créer les logs
+import urllib.request  # pour scrapper
+import smtplib  # pour les mails
+import pprint  # pretty print
+import logging  # pour créer les logs
+import django  # web framework python
+import pydoc  # génère les documentations
 
-import datetime #c'est assez évident non ?
+import datetime  # c'est assez évident non ?
 
 # pip freeze dans interpréteur pyhton permet d'avoir
 # toutes les dépendances del'environnement python directement
@@ -17,12 +19,12 @@ import datetime #c'est assez évident non ?
 now1 = datetime.datetime.today()
 now = datetime.date.today()
 print(now)
-back_then = datetime.date(1988,5,16)
+back_then = datetime.date(1988, 5, 16)
 ok_back = datetime.datetime.combine(back_then, datetime.time())
 
 print(ok_back)
 
-difference2 = (now1-ok_back).days
-difference = (now-back_then).days
+difference2 = (now1 - ok_back).days
+difference = (now - back_then).days
 print(difference)
 print(difference2)
